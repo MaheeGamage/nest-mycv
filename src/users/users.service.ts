c0181bc7895e4a1a1dfd6a77a20f878806f31a16
Mahee Gamage <mahee.gamage@gmail.com>
2022-12-10 03:56:38 +0000

    find user login implemented

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,12 +22,18 @@ export class UsersService {
 
     async update(id: number, attrs: Partial<User>) {
         const user = await this.findOne(id);
-        if(!user){
+        if (!user) {
             throw new Error('user not found');
         }
         Object.assign(user, attrs);
         return this.repo.save(user);
     }
 
-    remove() { }
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('user not found');
+        }
+        return this.repo.remove(user);
+    }
 }
