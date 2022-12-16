3ccd0d2da319fe9556439c89a2c22a44756ac1fe
MaheeGamage <mahee.gamage@gmail.com>
2022-12-16 22:13:59 +0530

    signin and signup implamented

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Patch, Param, Query, NotFoundException, UseInterceptors, ClassSerializerInterceptor, Delete } from '@nestjs/common';
 import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -9,11 +10,19 @@ import { UsersService } from './users.service';
 @Serialize(UserDto)
 export class UsersController {
 
-    constructor(private usersService: UsersService) { }
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService
+    ) { }
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body.email, body.password);
+        return this.authService.signup(body.email, body.password);
+    }
+
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto) {
+        return this.authService.signin(body.email, body.password);
     }
 
     @Get('/:id')
